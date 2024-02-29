517b2567e6be7804918a900bda54d2f3714c6d27
irotsunagu <admin@irotsunagu.com>
2024-03-01 00:11:23 +0900

    dark quest setting

--- src/containers/EditPinComponent.tsx
+++ src/containers/EditPinComponent.tsx
@@ -10,6 +10,8 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import '../App.css';
+
 /**
  * propsの定義
  */
